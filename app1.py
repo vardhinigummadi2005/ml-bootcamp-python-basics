dd2b91ca20def924296a0d804fe5bdff37f451b7
vardhinigummadi2005 <vardhinigummadi85@gmail.com>
2025-07-30 01:45:41 +0530

    uploading app1.py

---
+++ app1.py
@@ -0,0 +1,12 @@
+name="MLWEC"
+number=10.0
+isOn=True
+letter="H"
+# Arithmatic - +,-,*,/%
+#Logical - and,or,not
+#Comparison - <,>,==,!=,<=,>=
+#Assignment - =,+=,-=,*=,/=
+if (number>5):
+    print(name)
+else:
+    print(isOn)
